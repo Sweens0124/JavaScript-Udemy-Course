84047b9b49ef136d68017e0abe101cb002d4e400
Lucas Sweeney <sweens@Lucass-MacBook-Pro.local>
2022-03-18 22:02:31 -0600

    Second Commit

--- 03-Developer-Skills/starter/assignments.js
+++ 03-Developer-Skills/starter/assignments.js
@@ -1,12 +1,35 @@
-'use strict';
+"use strict";
 /*
 
 //          DEVELOPER SKILLS & EDITOR SETUP
 //      Coding challenge #1
 
-    Given an array of forecasted maximum temperatures, the thermometer
-displays a string with the given temperatures. 
-Ex: [17, 21, 23] will print "...17C in 1 days... 21C in 2 days... 23C in 3 days..."
-*/
+Given an array of forecasted maximum temperatures, the thermometer displays a string with the given temperatures:
 
-//  function 'printForecast' which takes an 'arr' and logs a string like the above^ to console.
+Ex: [17, 21, 23] will print "...17C in 1 days... 21C in 2 days... 23C in 3 days..."
+
+
+// function 'printForecast' which takes an 'arr' and logs a string like the above^ to console.
+// 1) the function takes in an array and logs a string 
+// 2) console log the function to string the above description
+// 3) x days? : index + 1
+
+const temps1 = [17, 21, 23];
+const temps2 = [12, 5, -5, 0, 4];
+console.log(temps1, temps2)
+
+
+// const printForecast = (arr) => {
+//     return `...${temps1[0]}C in 1 days... ${temps1[1]}C in 2 days... ${temps1[2]}C in 3 days...`
+// }
+// console.log(printForecast())
+
+const printForecast = (arr) => {
+    let str = '';
+    for(let i = 0; i < arr.length; i++) {
+        str += `${arr[i]}C in ${i + 1} days ...`;
+    }
+    console.log('...' + str)
+}
+printForecast(temps2);
+*/
--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -13,7 +13,7 @@ const calcAge = birthYear => 2037 - birthYear;
 //          INSTALLING NODE.JS AND SETTING UP A DEV ENVIRONMENT
 
 console.log(calcAge(1991));
-*/
+
 
 //          USING GOOGLE, STACKOVERFLOW, AND MDN WEB DOCS
 
@@ -29,7 +29,7 @@ const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 //  -Find max and min value in array
 //  -Subtract min from max (amplitude) and return it!
 
-const calcTempAmp = (temps) => {
+const calcTempAmp = function (temps) {
     let max = temps[0];
     let min = temps[0];
 
@@ -38,10 +38,84 @@ const calcTempAmp = (temps) => {
         if (typeof curTemp !== 'number') continue;
         if (curTemp > max) max = curTemp;
         if (curTemp < min) min = curTemp;
-    console.log(max, min)
     }
+    console.log(max, min);
     return max - min
 }
 const amplitude = calcTempAmp(temperatures); //runs through whole array til max is found
 console.log(amplitude);
 
+//  Problem 2
+//  function should now receive 2 arrays of temps
+//  1) understanding
+//  -with 2 arrays, do we need to implement functionality twice? NO! just merge two arrays
+//  2) breaking up into sub-probs
+//  - merge 2 arrays.
+// const array1 = ['a', 'b', 'c'];
+// const array2 = ['d', 'e', 'f'];
+// const array3 = array1.concat(array2);
+
+const calcTempAmpNew = function (t1, t2) {
+    const temps = t1.concat(t2)
+    console.log(temps);
+    let max = temps[0];
+    let min = temps[0];
+
+    for (let i = 0; i < temps.length; i++) {
+        const curTemp = temps[i]
+        if (typeof curTemp !== 'number') continue;
+        if (curTemp > max) max = curTemp;
+        if (curTemp < min) min = curTemp;
+    }
+    console.log(max, min);
+    return max - min
+}
+const amplitudeNew = calcTempAmpNew([3, 5, 1], [9, 0, 5]);
+console.log(amplitudeNew);
+
+//          DEBUGGING WITH THE CONSOLE AND BREAKPOINTS
+
+const measureKelvin = function() {
+    const measurement = {
+        type: 'temp',
+        unit: 'celsius',
+        // C) fix the bug
+        // value: Number(prompt('Degrees Celsius:'))
+        value: 10
+    };
+    console.log(measurement);
+
+    console.table(measurement);
+    // console.log(measurement.value);
+    // console.warn(measurement.value);
+    // console.error(measurement.value);
+
+    const kelvin = measurement.value + 273;
+    return kelvin;
+}
+// a) identify bug
+// b) find the bug.
+console.log(measureKelvin());
+
+//  Using a debugger
+const calcTempAmpBug = function (t1, t2) {
+    const temps = t1.concat(t2)
+    console.log(temps);
+    let max = 0;
+    let min = 0;
+
+    for (let i = 0; i < temps.length; i++) {
+        const curTemp = temps[i]
+        if (typeof curTemp !== 'number') continue;
+
+        // debugger;
+        if (curTemp > max) max = curTemp;
+        if (curTemp < min) min = curTemp;
+    }
+    console.log(max, min);
+    return max - min
+}
+const amplitudeBug = calcTempAmpBug([3, 5, 1], [9, 4, 5]);
+// a) identify the bug!
+console.log(amplitudeBug);
+*/
